41da4ff059ee72437727bf29f19734341370fee9
Samantha Kamal <71042204+SamanthaNKamal@users.noreply.github.com>
2020-10-08 21:40:48 -0400

    Update script.js

--- Lab4/script.js
+++ Lab4/script.js
@@ -30,8 +30,8 @@ let myFullName = "Samantha Kamal";
 // 4 & 5 Into the second paragraph tag, return the 3rd character in your first name
 // 6 Add a new line to your second paragraph
 // 7 Return the final three characters of your last name to that new line
-  var thirdCharacter = "The third character of my name is" +myFullName.charAt(2).toUpperCase();
-  newParagraphTwo.append(thirdCharacter);
+  var ThirdC = "The third character of my name is" +myFullName.charAt(2).toUpperCase();
+  newParagraphTwo.append(ThirdC);
 
   newParagraphTwo.append("\n");
   var LastThree = "The last three characters in my name are" + myFullName.substring(myFullName.length - 3, myFullName.length);
