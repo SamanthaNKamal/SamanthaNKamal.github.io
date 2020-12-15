a3c71ea3beb78f8362ad76ca29487305e5e0b7ea
skamal3 <samanthakamal1998@gmail.com>
2020-12-14 20:58:06 -0500

    update .sql

--- Assignment2/install_sqlite_tables.sql
+++ Assignment2/install_sqlite_tables.sql
@@ -4,10 +4,10 @@ CREATE TABLE IF NOT EXISTS  'people' ( "id" INTEGER PRIMARY KEY AUTOINCREMENT, "
 CREATE TABLE IF NOT EXISTS  'tasks' ( "id" INTEGER PRIMARY KEY AUTOINCREMENT, "taskName" VARCHAR  NOT NULL,   "dueDate" DATETIME, "groupId" INTEGER, "personId" INTEGER, "complete" BOOLEAN, "priority" INTEGER, FOREIGN KEY(groupId) REFERENCES groups (id),  FOREIGN KEY(personId) REFERENCES people (id) );
 INSERT INTO 'groups' (name) select "Company" WHERE NOT EXISTS (SELECT * FROM 'groups' WHERE name = "Company");
 INSERT INTO 'groups' (name) select "UMD" WHERE NOT EXISTS (SELECT * FROM 'groups' WHERE name ="UMD");
-INSERT INTO 'people' (firstName) select "Samantha" WHERE NOT EXISTS (SELECT * FROM 'people' WHERE firstName = "Samantha");
-INSERT INTO 'people' (firstName) select "Nikita" WHERE NOT EXISTS (SELECT * FROM 'people' WHERE firstName = "Nikita");
-INSERT INTO 'tasks' (taskName, dueDate, groupId, personId, complete, priority) select  "Submit INST 326 Final Project", "2020-12-14",3,1,0,1 WHERE NOT EXISTS (SELECT * FROM 'tasks' WHERE id = 1);
-INSERT INTO 'tasks' (taskName, dueDate, groupId, personId, complete, priority) select  "Submit INST 314 Unit 3 Projct", "2020-12-14",3,2,0,2 WHERE NOT EXISTS (SELECT * FROM 'tasks' WHERE id = 2);
-INSERT INTO 'tasks' (taskName, dueDate, groupId, personId, complete, priority) select  "Submit INST 335 Information-Enabled Change Project", "2020-12-11",1,1,0,2 WHERE NOT EXISTS (SELECT * FROM 'tasks' WHERE id = 3);
-INSERT INTO 'tasks' (taskName, dueDate, groupId, personId, complete, priority) select  "Submit INST 377 Assignmnet2", "2020-12-14",1,1,0,3 WHERE NOT EXISTS (SELECT * FROM 'tasks' WHERE id = 4);
-INSERT INTO 'tasks' (taskName, dueDate, groupId, personId, complete, priority) select  "Study For Finals", "2020-12-15",1,1,0,1 WHERE NOT EXISTS (SELECT * FROM 'tasks' WHERE id = 5);
+INSERT INTO 'people' (firstName) select "Bill F" WHERE NOT EXISTS (SELECT * FROM 'people' WHERE firstName = "Bill F");
+INSERT INTO 'people' (firstName) select "Andrew G" WHERE NOT EXISTS (SELECT * FROM 'people' WHERE firstName = "Andrew G");
+INSERT INTO 'tasks' (taskName, dueDate, groupId, personId, complete, priority) select  "INST377 Make Final", "2020-12-06",3,1,0,1 WHERE NOT EXISTS (SELECT * FROM 'tasks' WHERE id = 1);
+INSERT INTO 'tasks' (taskName, dueDate, groupId, personId, complete, priority) select  "INST377 Office Hours", "2020-12-03",3,2,0,2 WHERE NOT EXISTS (SELECT * FROM 'tasks' WHERE id = 2);
+INSERT INTO 'tasks' (taskName, dueDate, groupId, personId, complete, priority) select  "Wash Car", "2020-11-27",1,1,0,2 WHERE NOT EXISTS (SELECT * FROM 'tasks' WHERE id = 3);
+INSERT INTO 'tasks' (taskName, dueDate, groupId, personId, complete, priority) select  "Grocery Shop", "2020-11-30",1,1,0,3 WHERE NOT EXISTS (SELECT * FROM 'tasks' WHERE id = 4);
+INSERT INTO 'tasks' (taskName, dueDate, groupId, personId, complete, priority) select  "Pay Electric Bill", "2020-12-15",1,1,0,1 WHERE NOT EXISTS (SELECT * FROM 'tasks' WHERE id = 5);
